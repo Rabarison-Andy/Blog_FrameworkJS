5ec7b56cb83ac09a6546dfbd7619e9fe6dae5d28
Andy <124242004+Rabarison-Andy@users.noreply.github.com>
2025-12-04 15:44:35 +0100

    Update controller Commentaire

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,6 +3,7 @@ const Article = require('../models/Article');
 const AppError = require('../utils/AppError');
 const { catchAsync } = require('../middleware/errorHandler');
 
+// Create
 exports.createComment = catchAsync(async (req, res, next) => {
     const { articleId } = req.params;
 
@@ -24,6 +25,7 @@ exports.createComment = catchAsync(async (req, res, next) => {
     });
 });
 
+//Read
 exports.getCommentsByArticle = catchAsync(async (req, res, next) => {
     const { articleId } = req.params;
 
@@ -39,5 +41,98 @@ exports.getCommentsByArticle = catchAsync(async (req, res, next) => {
         success: true,
         count: comments.length,
         data: comments
+    });
+});
+
+exports.getCommentById = catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+
+    const comment = await Comment.findById(id);
+    
+    if (!comment) {
+        return next(new AppError('Commentaire non trouvé', 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        data: comment
+    });
+});
+
+exports.getApprovedCommentsByArticle = catchAsync(async (req, res, next) => {
+    const { articleId } = req.params;
+
+    // Vérifier que l'article existe
+    const article = await Article.findById(articleId);
+    if (!article) {
+        return next(new AppError('Article non trouvé', 404));
+    }
+
+    const comments = await Comment.findApprouvesByArticle(articleId);
+
+    res.status(200).json({
+        success: true,
+        count: comments.length,
+        data: comments
+    });
+});
+
+//Update
+exports.updateComment = catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+
+    const comment = await Comment.findByIdAndUpdate(
+        id,
+        req.body,
+        {
+            new: true,
+            runValidators: true
+        }
+    );
+
+    if (!comment) {
+        return next(new AppError('Commentaire non trouvé', 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        message: 'Commentaire mis à jour avec succès',
+        data: comment
+    });
+});
+
+exports.approveComment = catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+
+    const comment = await Comment.findById(id);
+    
+    if (!comment) {
+        return next(new AppError('Commentaire non trouvé', 404));
+    }
+
+    // Utiliser la méthode d'instance du modèle
+    await comment.approuver();
+
+    res.status(200).json({
+        success: true,
+        message: 'Commentaire approuvé avec succès',
+        data: comment
+    });
+});
+
+//Delete
+exports.deleteComment = catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+
+    const comment = await Comment.findByIdAndDelete(id);
+
+    if (!comment) {
+        return next(new AppError('Commentaire non trouvé', 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        message: 'Commentaire supprimé avec succès',
+        data: comment
     });
 });
